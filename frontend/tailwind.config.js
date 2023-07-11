219b1b6bf6b97c31c26f1ce4e649ceeaf4267ee0
balthazarely <balthazar.elyj@gmail.com>
2023-07-10 19:50:21 -0600

    lets go

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,17 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [require("daisyui")],
-  // theme: {
-  //   extend: {},
-  // },
-  // plugins: [require("daisyui")],
-  // daisyui: {
-  //   themes: [
-  //     {
-  //       lemonade: {
-  //         ...require("daisyui/src/theming/themes")["[data-theme=lemonade]"],
-  //       },
-  //     },
-  //   ],
-  // },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        lemonade: {
+          ...require("daisyui/src/theming/themes")["[data-theme=lemonade]"],
+        },
+      },
+    ],
+  },
 };
