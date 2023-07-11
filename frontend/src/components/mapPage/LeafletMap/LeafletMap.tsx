a02c005cea3eeaf01c38ac29e82f0b87ea937719
balthazarely <balthazar.elyj@gmail.com>
2023-07-10 20:39:24 -0600

    now mapbox is shit

--- frontend/src/components/mapPage/LeafletMap/LeafletMap.tsx
+++ frontend/src/components/mapPage/LeafletMap/LeafletMap.tsx
@@ -1,10 +1,10 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+// import "leaflet/dist/leaflet.css";
 
 export function LeafletMap() {
   return (
     <div className="border-4 border-fuchsia-600 h-96">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      {/* <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -14,7 +14,7 @@ export function LeafletMap() {
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
-      </MapContainer>
+      </MapContainer> */}
     </div>
   );
 }
